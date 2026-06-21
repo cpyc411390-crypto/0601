03122955cea41d30a63bf395eaf96507f6f3c9e8
cpyc411390-crypto <cpyc411390@hs.nycu.edu.tw>
2026-06-21 20:25:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ if "gemini_client" not in st.session_state:
 		api_key = st.secrets["GEMINI_API_KEY"]
 	except KeyError:
 		st.error("未找到 GEMINI_API_KEY")
-    	st.stop()
- 
+		st.stop()
+	
 	client = genai.Client(api_key=api_key)
 	st.session_state.gemini_client = client
  
